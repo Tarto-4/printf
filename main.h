cca0ba604608001248fc6af02a95606d50dfd060
Ahenfoa <izanrry@gmail.com>
2023-08-22 08:50:11 +0000

    main.h

---
+++ main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int _printf(const char *format, ...);
+void someOtherFunction();
+
+#define MAX_VALUE 100
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <unistd.h>
+
+
+#endif
+~                                                                                                                                     
+~                                                                                                                                     
+~                                                                                                                                     
+~    
