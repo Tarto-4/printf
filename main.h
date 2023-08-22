bd558b702f833383405eb8ccb7d343a2b0644329
Ahenfoa <izanrry@gmail.com>
2023-08-22 23:50:04 +0000

    main.h

--- main.h
+++ main.h
@@ -4,11 +4,14 @@
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
-
+#include <stdio.h>
+#include <stdlib.h>
 int _printf(const char *format, ...);
 int prnt_s(const char *str);
 int prnt_i(int num);
 int prnt_c(char c);
 int _putchar(char c);
 int print_unsigned_integer(unsigned int num);
+int print_char(va_list args);
+int print_string(va_list args);
 #endif
